675a1fc8d492df849e3c8c154d39f04c613edcd8
PolinB <PolinB2413@gmail.com>
2021-01-23 14:11:36 +0300

    Add register test with cypress and add commands for run tests in README.md

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# software-testing
+# Recipe book
+
+## Запуск тестов
+
+### e2e with playwright && unit && components
+```
+cd client
+npm test
+```
+
+### e2e with cypress 
+Для просмотра в браузере
+```
+npm run cypress
+```
+Для просмотра в консоли
+```
+npm test-cypress
+```
--- app/client/cypress/integration/test-file.spec.js
+++ app/client/cypress/integration/test-file.spec.js
@@ -1,16 +1,16 @@
 const PAGE_URL = 'http://localhost:3000';
 
-describe('Simple test', () => {
+describe('E2E tests with cypress', () => {
   it(`Welcome from home page`, () => {
     cy.visit(PAGE_URL);
     cy.get('#welcome').should('contain', 'Welcome to recipe book');
-  })
+  });
 
   it('To recipes page without user', () => {
     cy.visit(PAGE_URL + '/recipes');
     cy.get('h1').should('contain', 'Recipes');
     cy.get('#all-recipes').should('be.empty');
-  })
+  });
 
   it('Unregistered login', () => {
     let login = "a1234a"
@@ -22,7 +22,7 @@ describe('Simple test', () => {
     cy.get('#login-submit').click();
     cy.wait(100);
     cy.get('.error').should('not.be.empty').should('contain', 'Wrong login');
-  })
+  });
 
   it('Empty login', () => {
     cy.visit(PAGE_URL + '/login');
@@ -31,5 +31,32 @@ describe('Simple test', () => {
     cy.get('#login-submit').click();
     cy.wait(100);
     cy.get('.error').should('not.be.empty').should('contain', 'This is required');
-  })
+  });
+
+  it('Wrong name in register page', () => {
+    let login = "a1234a";
+    let firstName = '1234';
+    let lastName = 'BBBB';
+    let age = '11';
+    cy.visit(PAGE_URL + '/register');
+    cy.get('input#firstName').click({force: true});
+    cy.wait(100);
+    cy.get('input#firstName').type(firstName);
+    cy.wait(100);
+    cy.get('input#lastName').click({force: true});
+    cy.wait(100);
+    cy.get('input#lastName').type(lastName);
+    cy.wait(100);
+    cy.get('input#login').click({force: true});
+    cy.wait(100);
+    cy.get('input#login').type(login);
+    cy.wait(100);
+    cy.get('input#age').click({force: true});
+    cy.wait(100);
+    cy.get('input#age').type(age);
+    cy.wait(100);
+    cy.get('#register-submit').click();
+    cy.wait(100);
+    cy.get('.error').should('not.be.empty').should('contain', 'Use only Latin letters');
+  });
 })
